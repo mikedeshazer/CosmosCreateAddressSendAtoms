f51667ae910d3c2017200ece4e51e7669698bfcc
m <mike@proofsuite.com>
2019-07-12 16:43:56 +0900

    yolo

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
+console.log("Starting")
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+var request = require('request');
+var crypto = require('crypto');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const PORT = process.env.PORT || 5000
+var bodyParser = require("body-parser");
+var cors = require('cors');    
+
+
+var app = express();
+//var io = require('socket.io')(app);
+const http = require('http')
+var server = http.createServer(app).listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+var cors = require('cors');    
+app.use(cors({credentials: true, origin: '*'}));
+ // .set('views', path.join(__dirname, 'views'))
+  //.set('view engine', 'ejs')
+  //.get('/', (req, res) => res.render('pages/index'))
+ app.get("/", function(req, res) {
+
+ 	res.send("hello. Do stuff with cosmos");
+ })
+
+
+
+
+
+
+
+
